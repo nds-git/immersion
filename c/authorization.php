597b09711b7f2d7cc049eb9c79bf04768481d622
nds-git <noc.dima.inet@gmail.com>
2021-02-14 15:05:23 +0300

    Removed unnecessary check

--- c/authorization.php
+++ c/authorization.php
@@ -2,27 +2,19 @@
 session_start();
 include_once '../function.php';
 
-if(isset($_POST['authorization'])) {
- if( isset($_POST['email']) AND isset( $_POST['password']) )  //Если есть данные
- {
-  $email     = trim(htmlspecialchars($_POST['email']));  
-  $password  = trim(htmlspecialchars($_POST['password'])); 
+if( isset($_POST['email']) AND isset( $_POST['password']) )  //Если есть данные
+{
+ $email     = trim(htmlspecialchars($_POST['email']));  
+ $password  = trim(htmlspecialchars($_POST['password'])); 
     
-  $auth = authorization($email,$password);
+ $auth = authorization($email,$password);
 
-  if(!$auth) {
-   set_flash_message("danger","<strong>Уведомление!</strong> Пароль не верный.");
-   redirect_to ("./login.php");
-  }
-  else {
-   set_flash_message("success","<strong>Приветствую!</strong> На нашем сайте. ");
-   redirect_to ("../users.php");
-  }
+ if(!$auth) {
+  set_flash_message("danger","<strong>Уведомление!</strong> Пароль не верный.");
+  redirect_to ("./login.php");
+}
+ else {
+  set_flash_message("success","<strong>Приветствую!</strong> На нашем сайте. ");
+  redirect_to ("../users.php");
+}
  }//fin если существует $_POST['email']) AND isset( $_POST['password'])
-}// fin $_POST['login'] 
- 
-
-
-
-
-
--- c/register.php
+++ c/register.php
@@ -2,31 +2,24 @@
 session_start();
 include_once '../function.php';
 
- if(isset($_POST['login'])) {
-  if( isset($_POST['email']) AND isset( $_POST['password']) )  //Если есть данные
-  {
-    $email     = htmlspecialchars($_POST['email']);  
-    $password  = htmlspecialchars($_POST['password']); 
-
-    $user = get_user_by_email($email);
+if( isset($_POST['email']) AND isset( $_POST['password']) )  //Если есть данные
+{
+ $email     = htmlspecialchars($_POST['email']);  
+ $password  = htmlspecialchars($_POST['password']); 
+ $user = get_user_by_email($email);
     
-    // var_dump($user);die;
-
-    if(!empty($user)) {
-      set_flash_message("danger","<strong>Уведомление!</strong> Этот эл. адрес уже занят другим пользователем.");
-      redirect_to ("page_register.php");
-    }
-    else {
-    // var_dump($email,$password);die;
-    add_user($email,$password);
-     // var_dump($us);die;
-     set_flash_message("success","<strong>Поздравляем!</strong> Вы успешно зарегались.");
-     redirect_to ("login.php");
-    }
-  }//fin если существует $_POST['email']) AND isset( $_POST['password'])
- }// fin $_POST['login'] 
+ // var_dump($user);die;
+ if(!empty($user)) {
+  set_flash_message("danger","<strong>Уведомление!</strong> Этот эл. адрес уже занят другим пользователем.");
+  redirect_to ("page_register.php");
+ }
+ else {
+ // var_dump($email,$password);die;
+  add_user($email,$password);
+  // var_dump($us);die;
+  set_flash_message("success","<strong>Поздравляем!</strong> Вы успешно зарегались.");
+  redirect_to ("login.php");
+ }
+}//fin если существует $_POST['email']) AND isset( $_POST['password'])
  // else // Если данные не переданы
  //  echo "Данные не переданы!"; //Выводим сообщение об ошибке
-
-
-// $pdo -> close(); //это нужно или нет в pdo??
